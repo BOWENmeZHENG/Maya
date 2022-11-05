f0868a387f0f7be5c9b9f99aa3e72b3c5b184855
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-11-05 16:11:36 -0700

    curve fit

--- sen.py
+++ sen.py
@@ -1,8 +1,15 @@
 import utils
 
-
 col_var = ['WIDTH_mem', 'THICKNESS_mem', 'WIDTH_device', 'HEIGHT_chamber', 'HEIGHT_device', 'InnerRing', 'OuterRing']
 col_perf = ['Negative Pressure Adh Force', 'Pos Pressure Adh Force']
-data_var, data_perf = utils.read_data(6, col_var, col_perf)
-# utils.scatter(data_var['OuterRing'], data_perf['Negative Pressure Adh Force'])
 
+data_var, data_perf = utils.read_data(6, col_var, col_perf)
+
+def func(x, a, b, c):
+    return a * x ** 2 + b * x + c
+
+
+x_fit, yfit = utils.fitting(func, data_var['OuterRing'], data_perf['Negative Pressure Adh Force'])
+
+
+utils.scatter(data_var['OuterRing'], data_perf['Negative Pressure Adh Force'], x_fit, yfit)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
 
 
 def read_data(ind_up_to, col_var, col_perf):
@@ -12,8 +14,14 @@ def read_data(ind_up_to, col_var, col_perf):
     return data_var, data_perf
 
 
-def scatter(x, y):
+def scatter(x, y, x_fit, yfit):
     plt.figure(figsize=(8, 6))
-    plt.scatter(x, y)
+    plt.scatter(x, y, c='blue', s=10)
+    plt.plot(x_fit, yfit, c='red', linewidth=3)
     plt.show()
 
+
+def fitting(func, xdata, ydata):
+    popt, *_ = curve_fit(func, xdata, ydata)
+    x_fit = np.linspace(min(xdata), max(xdata), 50)
+    return x_fit, func(x_fit, *popt)
